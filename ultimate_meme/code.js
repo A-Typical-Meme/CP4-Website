1ec3335c74e49baebfbee2db94aae0140fc80135
Andrew-GR-The-Memestar <andrew.goodchild-remoundos18@alumni.loyola.ca>
2017-04-03 14:03:35 -0400

    Finialized JS Login
    Added external page, as well as a seperate Login page

--- ultimate_meme/code.js
+++ ultimate_meme/code.js
@@ -1,35 +1,54 @@
 function test() {
 	document.getElementById("test").innerHTML = "Success";
 }	
- 
+
+/* These are the profile settings, by username and password */
+
 var profile1 = ["goodchilda", "test"];
 var profile2 = ["john_derpy", "nope"];
+var key = "False"
+
+/* This function asks the user for their username and password by popup,
+Leaves these inputs in uservar and passvar for later */
+
+function login(){
+	while(key === "False"){
+		var uservar = prompt("Enter username: ")
+		var passvar = prompt("Enter password: ") 
+		profCheck(uservar, passvar)
+	}
+}
+ 
+/* This function cross checks the profile settings with the uservar and
+passvar inputs. It also returns the key variable as True so that it will not repeat after
+a successful login. */ 
  
 function profCheck(userInput, passInput){
-	var key = "False"
-	while(key == "False"){
-		if (userInput == profile1[0]){
-			if (passInput == profile1[1]){				
-				confirm("Login successful")
-				key = "True"
-			}
-			else {
-				confirm("Login not successful")
-			}
+	//This part checks profile1's username and password with the inputs give
+	if (userInput == profile1[0]){
+		if (passInput == profile1[1]){				
+			window.location = "file:///H:/Documents/GitHub/CP4-Website/ultimate_meme/ultimate_meme.html"
+			return(key = "True")
+			confirm("Login successful")
 		}
-		else if (userInput == profile2[0]){
-			if (passInput == profile2[1]){				
-				confirm("Login successful")
-				key = "True"
-			}
-			else {
-				confirm("Login not successful")
-			}
+		else {
+			confirm("Login not successful")
+		}
+
+	}
+	//This part checks profile2's username and password with the inputs given
+	else if (userInput == profile2[0]){
+		if (passInput == profile2[1]){			
+			window.location = "file:///H:/Documents/GitHub/CP4-Website/ultimate_meme/ultimate_meme.html"
+			return(key = "True")
+			confirm("Login successful")
 		}
 		else {
 			confirm("Login not successful")
 		}
 	}
+	else {
+		confirm("Login not successful")
+	}
+
 }
-
-
