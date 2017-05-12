27879a491809cc465f54d7bc88296a7825da38d0
Andrew-GR-The-Memestar <andrew.goodchild-remoundos18@alumni.loyola.ca>
2017-05-12 14:03:34 -0400

    LOCAL STORAGE ADDED
    Local storage systems successfully added

--- ultimate_meme/code.js
+++ ultimate_meme/code.js
@@ -2,31 +2,44 @@ function test() {
 	document.getElementById("test").innerHTML = "Success";
 }	
 
+function log() {
+	console.log(prof_sets)
+}
+
+function log_local() {
+	console.log(localStorage.new_prof_sets)
+}
+
+function clear() {
+	var bus = JSON.parse("new_prof_sets")
+	var bus = ""
+	localStorage.setItem("new_prof_sets", bus)
+}
+
 /* These are the profile settings, by username and password */
 
-var profile1 = {
-	username : "goodchilda", 
-	passWord : "test",
-};
+var profile1 = new person("goodchilda", "test")
 
-var profile2 = {
-	username : "john_derpy", 
-	passWord : "nope",
-};
+var profile2 = new person("john_derpy", "nope")
 
-/* The person condructor is a profile object creator for new users */
+var prof_sets = [profile1, profile2];
+
+/* The person constructor is a profile object creator for new users */
 
 function person (userIn, passIn){
 	this.username = userIn;
 	this.passWord = passIn;
 }
 
-/* This function links to the main database to create a new profile */
+/* This function links to the main database to create a new profile */ 
  
 var new_profile = function(){
 	userIn = prompt("Enter the new username: ");
 	passIn = prompt("Enter the new password: ");
-	prof_sets[prof_sets.length] = new person(userIn, passIn);
+	profile3 = new person(userIn, passIn);
+	prof_sets.push(profile3);
+	var sub = JSON.stringify(prof_sets)
+	localStorage.setItem("new_prof_sets", sub);
 	console.log(prof_sets[prof_sets.length - 1]);
 }
 
@@ -45,16 +58,15 @@ var login = function(){
 
 var key = "False";
 
-var prof_sets = [profile1, profile2,];
-
 /* This function cross checks the profile settings with the uservar and
 passvar inputs. It also returns the key variable as True so that it will not repeat after
 a successful login. */ 
  
 function profCheck(userInput, passInput){
-	for(i = 0; i <= prof_sets.length; i++ )
-		if(userInput === prof_sets[i].username){
-			if(passInput === prof_sets[i].passWord){
+	var new_prof_sets = JSON.parse(localStorage.new_prof_sets)
+	for(i = 0; i <= new_prof_sets.length; i++ )
+		if(userInput === new_prof_sets[i].username){
+			if(passInput === new_prof_sets[i].passWord){
 				window.location = "file:///H:/Documents/GitHub/CP4-Website/ultimate_meme/ultimate_meme.html"
 				return(key = "True")
 				confirm("Login successful")
@@ -63,32 +75,5 @@ function profCheck(userInput, passInput){
 			confirm("Login not successful")
 			}
 		}
-	//This part checks profile1's username and password with the inputs give
-	/*if (userInput == profile1[0]){
-		if (passInput == profile1[1]){				
-			window.location = "file:///H:/Documents/GitHub/CP4-Website/ultimate_meme/ultimate_meme.html"
-			return(key = "True")
-			confirm("Login successful")
-		}
-		else {
-			confirm("Login not successful")
-		}
-
-	}
-	//This part checks profile2's username and password with the inputs given
-	else if (userInput == profile2[0]){
-		if (passInput == profile2[1]){			
-			window.location = "file:///H:/Documents/GitHub/CP4-Website/ultimate_meme/ultimate_meme.html"
-			return(key = "True")
-			confirm("Login successful")
-		}
-		else {
-			confirm("Login not successful")
-		}
-	}
-	else {
-		confirm("Login not successful") 
-	
-	}*/
 
 }
